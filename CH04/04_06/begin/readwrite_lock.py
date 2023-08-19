4a19700f7fbeefc23721bb3e7d1d22c5af10a827
ashutoshsameer <sameerashutosh0201@gmail.com>
2023-08-19 16:05:25 -0400

    ReadWrite Lock

--- CH04/04_06/begin/readwrite_lock.py
+++ CH04/04_06/begin/readwrite_lock.py
@@ -2,27 +2,30 @@
 """ Several users reading a calendar, but only a few users updating it """
 
 import threading
+from readerwriterlock import rwlock
 
 WEEKDAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 today = 0
-marker = threading.Lock()
+marker = rwlock.RWLockFair()
 
 def calendar_reader(id_number):
     global today
+    read_marker = marker.gen_rlock()
     name = 'Reader-' + str(id_number)
     while today < len(WEEKDAYS)-1:
-        marker.acquire()
-        print(name, 'sees that today is', WEEKDAYS[today])
-        marker.release()
+        read_marker.acquire()
+        print(name, 'sees that today is', WEEKDAYS[today], '-read count:', read_marker.c_rw_lock.v_read_count)
+        read_marker.release()
 
 def calendar_writer(id_number):
     global today
+    write_marker = marker.gen_wlock()
     name = 'Writer-' + str(id_number)
     while today < len(WEEKDAYS)-1:
-        marker.acquire()
+        write_marker.acquire()
         today = (today + 1) % 7
         print(name, 'updated date to ', WEEKDAYS[today])
-        marker.release()
+        write_marker.release()
 
 if __name__ == '__main__':
     # create ten reader threads
