c1c183c9a49a9c46e91765c174ecf602b22d60ef
Ashutosh Sameer <sameerashutosh0201@gmail.com>
2023-08-20 18:12:06 -0400

    Live Lock

--- CH05/05_08/begin/livelock.py
+++ CH05/05_08/begin/livelock.py
@@ -2,24 +2,30 @@
 """ Three philosophers, thinking and eating sushi """
 
 import threading
+import time
+from random import random
 
 chopstick_a = threading.Lock()
 chopstick_b = threading.Lock()
 chopstick_c = threading.Lock()
 sushi_count = 500
 
-def philosopher(name, first_chopstick, second_chopstick):
+def philosopher(name, first_chopstick: threading.Lock, second_chopstick: threading.Lock):
     global sushi_count
     while sushi_count > 0: # eat sushi until it's all gone
         first_chopstick.acquire()
-        second_chopstick.acquire()
-        try:
-            if sushi_count > 0:
-                sushi_count -= 1
-                print(name, 'took a piece! Sushi remaining:', sushi_count)
-        finally:
-            second_chopstick.release()
+        if not second_chopstick.acquire(blocking=False):
+            print(name, 'released their first chopstick.')
             first_chopstick.release()
+            time.sleep(random()/10)
+        else:
+            try:
+                if sushi_count > 0:
+                    sushi_count -= 1
+                    print(name, 'took a piece! Sushi remaining:', sushi_count)
+            finally:
+                second_chopstick.release()
+                first_chopstick.release()
 
 if __name__ == '__main__':
     threading.Thread(target=philosopher, args=('Barron', chopstick_a, chopstick_b)).start()
